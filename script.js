8c8b6ff12597c2b473864565f6432cbdb49bf385
Vu Ba Tien <tienvb@mirabo.com.vn>
2020-10-27 16:15:03 +0700

    add demo 3d object

--- script.js
+++ script.js
@@ -5,40 +5,93 @@ window.onload = () => {
 
 function staticLoadPlaces() {
    return [
+    //    {
+    //        name: 'place',
+    //        src: 'place.png',
+    //        location: {
+    //            lat: 21.006933,
+    //            lng: 105.843174,
+    //        }
+    //    },
+    //    {
+    //         name: 'robot',
+    //         src: 'robot.jpg',
+    //         location: {
+    //             lat: 21.006347,
+    //             lng: 105.842388,
+    //         }
+    //     },
+    //     {
+    //         name: 'dog',
+    //         src: 'dog.png',
+    //         location: {
+    //             lat: 21.006568,
+    //             lng: 105.843839,
+    //         }
+    //     },
+    //     {
+    //         name: 'tree',
+    //         src: 'tree.png',
+    //         location: {
+    //             lat: 20.997403,
+    //             lng: 105.843586,
+    //         }
+    //     },
        {
-           name: 'place',
-           src: 'place.png',
-           location: {
-               lat: 21.006933,
-               lng: 105.843174,
-           }
-       },
-       {
-            name: 'robot',
-            src: 'robot.jpg',
+            name: 'Magnemite',
             location: {
-                lat: 21.006347,
-                lng: 105.842388,
-            }
+                lat: 21.009362,  
+                lng: 105.857678,
+            },
+            url: './assets/articuno/scene.gltf',
+            scale: '1 1 1',
+            rotation: '0 180 0',
+            info: 'Magnemite, Lv. 5, HP 10/10',
         },
         {
-            name: 'dog',
-            src: 'dog.png',
+            name: 'Articuno',
             location: {
-                lat: 21.006568,
-                lng: 105.843839,
-            }
+                lat: 21.004194, 
+                lng: 105.854113,
+            },
+            url: './assets/articuno/scene.gltf',
+            scale: '1 1 1',
+            rotation: '0 180 0',
+            info: 'Articuno, Lv. 80, HP 100/100',
         },
         {
-            name: 'tree',
-            src: 'tree.png',
+            name: 'Dragonite',
             location: {
-                lat: 20.997403,
-                lng: 105.843586,
-            }
+                lat: 21.004434,  
+                lng: 105.860555,
+            },
+            url: './assets/articuno/scene.gltf',
+            scale: '1 1 1',
+            rotation: '0 180 0',
+            info: 'Dragonite, Lv. 99, HP 150/150',
         },
    ];
 }
+
+var setModel = function (model, entity) {
+    if (model.scale) {
+        entity.setAttribute('scale', model.scale);
+    }
+
+    if (model.rotation) {
+        entity.setAttribute('rotation', model.rotation);
+    }
+
+    if (model.position) {
+        entity.setAttribute('position', model.position);
+    }
+
+    entity.setAttribute('gltf-model', model.url);
+
+    // const div = document.querySelector('.instructions');
+    // div.innerText = model.info;
+};
+
 
 function renderPlaces(places) {
    let scene = document.querySelector('a-scene');
@@ -46,21 +99,31 @@ function renderPlaces(places) {
    places.forEach((place) => {
        let latitude = place.location.lat;
        let longitude = place.location.lng;
-       let src = place.src
+    // Image show
+    //    let src = place.src
+    //    let img = document.createElement('a-image');
+    //    img.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    //    img.setAttribute('src', `${src}`);
+    //    img.setAttribute('look-at', '[gps-camera]');
+    //    img.setAttribute('scale', '25 25 25');
+    //    scene.appendChild(img);
 
-       let img = document.createElement('a-image');
-       img.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       img.setAttribute('src', `${src}`);
-       img.setAttribute('look-at', '[gps-camera]');
-       img.setAttribute('scale', '25 25 25');
-    //    img.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-    //    img.setAttribute('rotation', '0 180 0');
-    //    img.setAttribute('animation-mixer', '');
-       
-    //    model.addEventListener('loaded', () => {
-    //        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-    //    });
+    // Entity model show
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
 
-       scene.appendChild(img);
+        setModel(place, model);
+
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('look-at', '[gps-camera]');
+
+        // document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+        //     var entity = document.querySelector('[gps-entity-place]');
+        //     modelIndex++;
+        //     var newIndex = modelIndex % models.length;
+        //     setModel(models[newIndex], entity);
+        // });
+
+        scene.appendChild(model);
    });
 }
